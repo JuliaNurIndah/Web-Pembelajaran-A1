fb39b3a9ea51fd2dc4a0ed029be265da73513935
JuliaNurIndah <125952157+JuliaNurIndah@users.noreply.github.com>
2023-05-24 11:32:29 +0700

    Add files via upload

---
+++ HELLOWORLD.js
@@ -0,0 +1,29 @@
+document.writeln("Hello World");
+//Perkenalan
+document.writeln("<p> JULIA NUR INDAH SARI </p>");
+document.writeln("</br>");
+document.writeln("<p> NIM 20105241007 </p>");
+document.writeln("<p> Kelas : A1 </p>");
+document.writeln(0b10101);
+document.writeln("<p>");
+
+document.writeln("NAMA : "+"Julia");
+document.writeln("</br>");
+document.writeln("Nama : Julia");
+document.writeln("</br>");
+
+var fullName = "Julia Nur Indah Sari";
+var firstName = "Julia";
+var middleName = "Indah";
+var lastName = "Sari";
+
+document.writeln("Nama Lengkap : "+ fullName);
+document.writeln("</br>");
+document.writeln("Nama Lengkap : "+ firstName);
+document.writeln("</br>");
+document.writeln("Nama Lengkap : "+ middleName);
+document.writeln("</br>");
+document.writeln("Nama Lengkap : "+ lastName);
+
+
+
